290a7bda912f5198c7de69ccbc507074a26672d1
Truong Quang Huy <61649070+kzyo5962@users.noreply.github.com>
2022-12-01 15:51:33 +0700

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authApi from "./authApi";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://demo-backend-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
